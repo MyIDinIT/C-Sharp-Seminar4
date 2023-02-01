829bd158868aad579c10daf8b53ad88d24518b2e
IvanZH <ivzh.nn@gmail.com>
2023-02-01 16:43:55 +0300

    task 26 completed

---
+++ task26/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число
+//  и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число:");
+int a=Convert.ToInt32(Console.ReadLine());
+
+if (a<0) a=-a;
+
+string b=Convert.ToString(a);
+ 
+int X=b.Length;
+
+Console.WriteLine("Длина вашего числа составляет " + X);
